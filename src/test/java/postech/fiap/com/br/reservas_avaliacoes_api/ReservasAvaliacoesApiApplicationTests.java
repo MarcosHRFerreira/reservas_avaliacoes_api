fe0c8895dc60033d0a7e8c1c303dc7273209fd01
Marcos Ferreira <marcoshrferreira@gmail.com>
2024-08-14 10:05:03 -0300

    Carga inicial

---
+++ src/test/java/postech/fiap/com/br/reservas_avaliacoes_api/ReservasAvaliacoesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package postech.fiap.com.br.reservas_avaliacoes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasAvaliacoesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
